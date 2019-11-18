317928461a903c2dda87ff9163081359ea83a6b0
rajeeshqb <rajeesh@qburst.com>
2019-11-18 17:26:20 +0530

    license text, version, project name updated

--- src/SocketConnection.js
+++ src/SocketConnection.js
@@ -1,3 +1,24 @@
+/****************************************************************************************
+
+    Striking is a simulation of a carrom board play where 2 players can 
+    play carrom board (board version) using a browser. 
+
+    Copyright © 2019  QBurst Technologies 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt />. 
+
+****************************************************************************************/
+
 export default class SocketConnection {
   constructor(url) {
     let socket = io(url);
